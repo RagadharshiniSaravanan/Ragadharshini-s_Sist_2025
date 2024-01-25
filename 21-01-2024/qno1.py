7d88c22b4aff80154a3b7aecddb52f3fba0c45a7
RagadharshiniSaravanan <157377709+RagadharshiniSaravanan@users.noreply.github.com>
2024-01-25 11:49:58 +0530

    Create qno1.py

---
+++ 21-01-2024/qno1.py
@@ -0,0 +1,13 @@
+if _name_ == '_main_':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+
+    #logic 2:
+
+
+a = int(input())
+b = int(input())
+print (a//b)
+print (a/b)
